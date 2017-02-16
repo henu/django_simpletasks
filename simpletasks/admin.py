2ff24be05ccc61081c58c887a7b7f87391c01292
Henrik Heino <henrik.heino@gmail.com>
2017-02-16 20:02:23 +0700

    More informative admin panel.

--- simpletasks/admin.py
+++ simpletasks/admin.py
@@ -6,3 +6,5 @@ from simpletasks.models import Task
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     readonly_fields = ['func', 'args', 'start_at', 'expires_at', 'timeout', 'started_at', 'finished_at', 'error', 'output', 'scheduled_task']
+
+    list_display = ['__unicode__', 'started_at', 'finished_at', 'error']
--- simpletasks/models.py
+++ simpletasks/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 
 class ScheduledTask(models.Model):
     label = models.CharField(max_length=250)
+
+    def __unicode__(self):
+        return self.label
 
 
 class Task(models.Model):
@@ -22,7 +26,24 @@ class Task(models.Model):
     scheduled_task = models.ForeignKey(ScheduledTask, related_name='tasks')
 
     def __unicode__(self):
-        return self.func
+        if self.finished_at:
+            if self.error:
+                status = u'error'
+            else:
+                status = u'finished'
+        elif self.started_at:
+            ago = (timezone.now() - self.started_at).total_seconds()
+            if ago < 60:
+                status = u'started {} seconds ago'.format(int(ago))
+            elif ago < 60*60:
+                status = u'started {} minutes ago'.format(int(ago/60))
+            elif ago < 60*60*24:
+                status = u'started {} hours ago'.format(int(ago/60/60))
+            else:
+                status = u'started {} days ago'.format(int(ago/60/60/24))
+        else:
+            status = u'not started'
+        return u'{} ({})'.format(self.func, status)
 
 
 class Process(models.Model):
